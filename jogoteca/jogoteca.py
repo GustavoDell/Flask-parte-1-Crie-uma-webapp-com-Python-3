57e85eb2ac75c45fa8d6c3a26a9985faca7e6fa3
GustavoDell <gustavo.dell@outlook.com>
2020-02-05 00:18:44 -0300

    Pegando lista do servidor

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/inicio")
-def ola(): 
-    return render_template('lista.html', titulo="Jogos")
+def ola():
+    lista = ["Tetris", "Super Mario", "Pokemon Gold"] 
+    return render_template('lista.html', titulo="Jogos", jogos=lista)
 
 app.run(host='127.0.0.1', port=8080)
