ae52118021ea62d4a3c769330ee77ac2b46ccbd5
GustavoDell <gustavo.dell@outlook.com>
2020-02-10 21:05:41 -0300

    06. Implementando autorização para criar Jogos

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
 app.secret_key = "gustavo"
@@ -8,6 +8,17 @@ class Jogo:
         self.nome = nome
         self.categoria = categoria
         self.console = console
+
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+usuario1 = Usuario("Gusta", "Gustavo", "1234")
+usuario2 = Usuario("Fulano", "Fulano", "56")
+
+usuarios = {usuario1.id: usuario1, usuario2.id: usuario2}
 
 jogo1 = Jogo("Super Mario", "Ação", "SNES")
 jogo2 = Jogo("Pokemon Golf", "RPG", "GBA")
@@ -21,7 +32,7 @@ def index():
 @app.route("/novo")
 def novo():
     if("usuario_logado" not in session or session['usuario_logado'] == None):
-        return redirect("/login?proxima=novo")
+        return redirect(url_for('login', proxima=url_for('novo')))
     else:
         return render_template("novo.html", titulo="Novo Jogo")
 
@@ -34,7 +45,7 @@ def criar():
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
 
-    return redirect("/")
+    return redirect(url_for('index'))
 
 @app.route("/login")
 def login():
@@ -43,18 +54,24 @@ def login():
 
 @app.route("/autenticar", methods=["POST",])
 def autenticar():
-    if "mestra" == request.form["senha"]:
-        session['usuario_logado'] = request.form['usuario'] #session armazena dados do usuario por mais de um ciculo de request
-        flash(request.form["usuario"] + " logou com sucesso!")#messagem flash. Mostra a rapido a mensagem
-        return redirect("/")
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id #session armazena dados do usuario por mais de um ciculo de request
+            flash(usuario.nome + " logou com sucesso!")#messagem flash. Mostra a rapido a mensagem
+            proxima_pagina = request.form["proxima"]
+            return redirect(proxima_pagina)
+        else:
+            flash("Senha incorreta!")
+            return redirect(url_for('login'))
     else:
-        flash("Não logado, tente novamente!")
-        return redirect("/login")
+        flash("Usuario não cadastrado!")
+        return redirect(url_for('login'))
 
 @app.route("/logout")
 def logout():
     session["usuario_logado"] = None
     flash("Usuario deslogado")
-    return redirect("/")
+    return redirect(url_for('index'))
 
 app.run(host='127.0.0.1', port=8080, debug=True)
